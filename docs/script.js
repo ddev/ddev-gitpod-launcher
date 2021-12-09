d7799500aab803db2b2b7c92a9a1a29568545670
Theodoros Ploumis <me@theodorosploumis.com>
2021-12-09 23:11:42 +0200

    Allow artifacts field to be empty

--- docs/script.js
+++ docs/script.js
@@ -14,30 +14,22 @@ document.body.onload = () => {
         }
         urlString += encodeURIComponent(key)+'='+encodeURIComponent(values[key]);
       });
-      
-      
+
       const owner = window.location.host.split('.')[0]
       const baseRepo = `https://github.com/` + owner + window.location.pathname
       urlString += "/" + baseRepo
-      
+
       document.getElementById('computedUrl').setAttribute('href', urlString);
       document.getElementById('ddev-link').innerHTML = urlString;
     })
   }
-  const updateArtifacts = () => {
-    sourceField = document.getElementById("ddev-repo")
-    artifactsField = document.getElementById("ddev-artifacts")
-    artifactsField.value = sourceField.value + "-artifacts"
-  }
-  updateArtifacts();
+
   computeLink();
+
   document.getElementById('target').querySelectorAll('[name]').forEach((element) => {
     element.addEventListener('change', (e) => {
       computeLink();
     })
   })
-  document.getElementById('ddev-repo').addEventListener('change', (e) => {
-      updateArtifacts();
-    })
 
 }
